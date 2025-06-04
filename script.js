b0b6add1abbe62d5a90d3cfa510f1453758c969f
Yuri Viana <ycviana@sga.pucminas.br>
2025-06-04 08:21:37 -0300

    feat: adicionar dinamismo ao input do tipo file com JS

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const onImg = document.querySelector('.online-img');
 const colorPickers = document.querySelectorAll('.color-picker');
 const check = document.getElementById('light');
 const label = document.querySelector('.light-type');
+const inputFile = document.getElementById('file');
+const error = document.querySelector('.error');
 
 function alterarImg(link, caminho) {
     link.src = `assets/icons/${caminho}`;
@@ -38,4 +40,23 @@ colorPickers.forEach(picker => {
 
 check.addEventListener('change', () => {
     label.innerHTML = check.checked ? 'Claro' : 'Escuro';
+})
+
+error.style.opacity = '0';
+
+inputFile.addEventListener('change', () => {
+    const file = inputFile.files[0];
+    const nameFile = document.querySelector('.file-select');
+    const name = file.name;
+
+    nameFile.innerHTML = name;
+
+    if (file) {
+    if (!file.type.match('image/png') && !file.type.match('image/jpeg')) {
+        alert('Por favor, insira uma imagem PNG ou JPEG.');
+        error.style.opacity = '1';
+        inputFile.value = '';
+        return;
+    }
+}
 })
